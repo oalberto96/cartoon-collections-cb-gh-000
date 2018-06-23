15f122a0b666cd9a056b7c0fcda6fe0db4ea5511
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 01:11:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(elements)# code an argument here
   elements.collect {|element| element.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|e| e.size < } 
 end
 
 def find_the_cheese# code an argument here
