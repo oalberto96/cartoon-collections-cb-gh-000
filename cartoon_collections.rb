af8d07a60b90a77c47d2a0d22d084194708ae17a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 01:06:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
-  dwarfs.each_with_index {|dwarf, index| puts "#{index} #{dwarf}" }
+  dwarfs.each_with_index {|dwarf, index| puts "#{index + 1} #{dwarf}" }
 end
 
 def summon_captain_planet# code an argument here
