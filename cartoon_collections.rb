c4941c853019b6d3f70f3a9bff8a87a90a4a465a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 01:11:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.any? {|e| e.size < } 
+  array.any? {|e| e.size > 4}
 end
 
 def find_the_cheese# code an argument here
