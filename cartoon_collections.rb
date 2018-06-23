65f8a3eab808bb462669249dcea9e5798df1167a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 01:17:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|e| e.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each{|item| cheese_types.include?(item) ? return item}
 end
